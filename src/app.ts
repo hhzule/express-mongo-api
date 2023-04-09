13f5b33ee8a55b0dbcb8982db0e725eebec0f7f7
hhzule <hhzule@gmail.com>
2023-04-09 23:32:04 +0500

    tests added

--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,10 @@ import swaggerDocs from "./utils/swagger";
 import createServer from "./utils/server";
 
 const port = config.get<number>('port')
-
 export const app = createServer()
-
 
 app.listen(port, async () => {
     logger.info(`App is running at ${port}`)
     await connect()
-
     swaggerDocs(app, port)
 })
