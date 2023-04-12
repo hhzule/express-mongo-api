54f93f6263b5032789a02a68339b8bf2f8757196
hhzule <hhzule@gmail.com>
2023-04-12 05:05:53 +0500

    swagger paths updated

--- build/config/default.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require('dotenv').config();
-exports.default = {
-    port: 1387,
-    dbUri: process.env.DB_URI
-};
--- build/config/production.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    port: 1387,
-    dbUri: process.env.DB_URI
-};
