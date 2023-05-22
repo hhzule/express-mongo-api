21550e53786100f35865a06a89bac7bda085dffd
ali ahmed <extricateshareware@gmail.com>
2023-05-22 21:57:25 +0500

    middleware wallet generation added

---
+++ src/middleware/walleteGenerate.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from 'express';
+import { ethers } from 'ethers';
+import bcrypt from 'bcrypt';
+
+// Middleware function
+function walleteGenerate(req: Request, res: Response, next: NextFunction) {
+  try {
+    // Generate a new wallet
+    const wallet = ethers.Wallet.createRandom();
+
+    // Encrypt the private key
+    const encryptedPrivateKey = bcrypt.hashSync(wallet.privateKey, 10);
+
+    // Append wallet and encrypted private key to req.body
+    req.body.walletAddress = wallet.address;
+    req.body.encryptedPrivateKey = encryptedPrivateKey;
+
+    // Call the next middleware function
+    next();
+  } catch (error) {
+    // Handle any errors
+    res.status(500).json({ error: 'Internal server error' });
+  }
+}
+
+export default walleteGenerate;
