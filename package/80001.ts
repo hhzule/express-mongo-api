a7c546a9e3272b79e028723649903aee9b97447e
ali ahmed <extricateshareware@gmail.com>
2023-05-29 14:45:06 +0500

    package testnet added

---
+++ package/80001.ts
@@ -0,0 +1,6 @@
+
+  const address_80001 = {
+    token_addr : "0x90B72a8a1259f3Cd493670639E03fdabF252560b" ,
+    NFT_addr : "0x8Eb6B4D40D35243352aBAD59BFDB27a161F3Bdc1" 
+   } 
+  export { address_80001 };
