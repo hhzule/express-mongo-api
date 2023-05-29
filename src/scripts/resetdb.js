a66444795176667e7aa2a42da270ab3c7ec3805e
hhzule <hhzule@gmail.com>
2023-05-29 18:40:00 +0500

    send

--- src/scripts/resetdb.js
+++ src/scripts/resetdb.js
@@ -1,45 +1,40 @@
-var CustomerModel = require("../models/customer.model");
-var DealerModel = require("../models/dealer.model");
-var AdminModel = require("../models/admin.model");
-var TransactionModel = require("../models/transaction.model");
-var WatchModel = require("../models/watch.model");
+var CustomerModel = require("../../build/src/models/customer.model");
+var DealerModel = require("../../build/src/models/dealer.model");
+var AdminModel = require("../../build/src/models/admin.model");
+var TransactionModel = require("../../build/src/models/transaction.model");
+var WatchModel = require("../../build/src/models/watch.model");
 
 const functionReset = async () => {
   try {
     /**MongoDb call */
-    let Admin = await AdminModel.deleteMany();
+    let Admin = await AdminModel.deleteMany({});
   } catch (e) {
-    logger.error(e);
-    return res.status(409).send(e.message);
+    console.log(e);
   }
-  try {
-    /**MongoDb call */
-    let Customer = await CustomerModel.deleteMany();
-  } catch (e) {
-    logger.error(e);
-    return res.status(409).send(e.message);
-  }
-  try {
-    /**MongoDb call */
-    let Dealer = await DealerModel.deleteMany();
-  } catch (e) {
-    logger.error(e);
-    return res.status(409).send(e.message);
-  }
-  try {
-    /**MongoDb call */
-    let Transaction = await TransactionModel.deleteMany();
-  } catch (e) {
-    logger.error(e);
-    return res.status(409).send(e.message);
-  }
-  try {
-    /**MongoDb call */
-    let Watch = await WatchModel.deleteMany();
-  } catch (e) {
-    logger.error(e);
-    return res.status(409).send(e.message);
-  }
+  //   try {
+  //     /**MongoDb call */
+  //     let Customer = await CustomerModel.remove({});
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  //   try {
+  //     /**MongoDb call */
+  //     let Dealer = await DealerModel.remove({});
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  //   try {
+  //     /**MongoDb call */
+  //     let Transaction = await TransactionModel.remove({});
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  //   try {
+  //     /**MongoDb call */
+  //     let Watch = await WatchModel.remove({});
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
   console.log("first");
 };
 
