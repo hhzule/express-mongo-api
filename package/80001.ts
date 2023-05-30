1dbaa25e438ecc82fedeabdaeeef49051803d6d2
ali ahmed <extricateshareware@gmail.com>
2023-05-30 09:44:42 +0500

    asssd

--- package/80001.ts
+++ package/80001.ts
@@ -1,6 +1,6 @@
 
   const address_80001 = {
-    token_addr : "0x90B72a8a1259f3Cd493670639E03fdabF252560b" ,
-    NFT_addr : "0x8Eb6B4D40D35243352aBAD59BFDB27a161F3Bdc1" 
+    token_addr : "0xce7C4347b3222e24761f75f7837f37A67CD8fDeD" ,
+    NFT_addr : "0xB9De4b012F3620cb023d432B77Ccc2E88b98502E" 
    } 
   export { address_80001 };
